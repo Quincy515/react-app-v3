1f1ca8f5bd537ca3dc04430aa9c51c5c787e9072
custertian <custertian@users.noreply.github.com>
2018-07-04 13:24:16 +0800

    10.mobx配置

---
+++ client/store/app-state.js
@@ -0,0 +1,37 @@
+import {
+  observable,
+  computed,
+  autorun,
+  action,
+} from 'mobx'
+
+export class AppState {
+  @observable count = 0
+
+  @observable name = 'wangbadan'
+
+  @computed get msg() {
+    return `${this.name} say count is  ${this.count}`
+  }
+
+  @action add() {
+    this.count += 1
+  }
+
+  @action changeName(name) {
+    this.name = name
+  }
+}
+
+const appState = new AppState()
+
+autorun(() => {
+  console.log(appState.msg) // eslint-disable-line
+})
+
+setInterval(() => {
+  // appState.count += 1
+  appState.add()
+}, 2000)
+
+export default appState
